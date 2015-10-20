a94006b85773d1531a0cd8c23e24b20a0572773f
Weijie <aaronlin519@yahoo.com>
2015-10-19 22:15:01 -0700

    frontPageadded Signin Signup form

--- gameStart/static/scripts/snakes.js
+++ gameStart/static/scripts/snakes.js
@@ -2,9 +2,11 @@ var Frontpage = (function() {
 	var attachHomeHandler = function(e){
 		$('.active').click(function(e){
 			e.preventDefault();
-			$('.gamerule').hide();
-			$('.intro').hide();
-			$('.leaderboard').hide();
+			$('.gamerule').fadeOut(1000);
+			$('.intro').fadeOut(1000);
+			$('.login_container').fadeOut(1000);
+			$('.leaderboard').fadeOut(1000);
+			$('.signup_container').hide();
 		});
 	};
 	var attachGameRuleHandler = function(e) {
@@ -12,7 +14,9 @@ var Frontpage = (function() {
 			e.preventDefault();
 			$('.leaderboard').hide();
 			$('.intro').hide();
-			$('.gamerule').show();
+			$('.login_container').hide();
+			$('.signup_container').hide();
+			$('.gamerule').fadeIn(1000);
 		});
 	};
 	var attachIntroductionHandler = function(e){
@@ -20,7 +24,9 @@ var Frontpage = (function() {
 			e.preventDefault();
 			$('.gamerule').hide();
 			$('.leaderboard').hide();
-			$('.intro').show();
+			$('.login_container').hide();
+			$('.signup_container').hide();
+			$('.intro').fadeIn(1000);
 		});
 	};
 	var attachLeaderboardHandler = function(e){
@@ -28,17 +34,45 @@ var Frontpage = (function() {
 			e.preventDefault();
 			$('.gamerule').hide();
 			$('.intro').hide();
-			$('.leaderboard').show();
+			$('.login_container').hide();
+			$('.signup_container').hide();
+			$('.leaderboard').fadeIn(1000);
+		});
+	};
+	var attachStartGameHandler = function(e){
+		$('.gamestart-button').click(function(e){
+			e.preventDefault();
+			$('.gamerule').hide();
+			$('.intro').hide();
+			$('.leaderboard').hide();
+			$('.signup_container').hide();
+			$('.login_container').fadeIn(1000);
+		});
+	};
+	var attachSignUpHandler = function(e){
+		$('.text-center').click(function(e){
+			e.preventDefault();
+			$('.login_container').hide();
+			$('.signup_container').fadeIn(1000);
 		});
 	};
 	var start = function(){
+		$('.logo').hide();
+		$('#cssmenu').hide();
+		$('.logo').fadeToggle(1000);
+		$('#cssmenu').fadeIn(2000);
 		$('.gamerule').hide();
 		$('.intro').hide();
+		$('.signup_container').hide();
 		$('.leaderboard').hide();
+		$('.login_container').hide();
 		attachGameRuleHandler();
 		attachHomeHandler();
 		attachIntroductionHandler();
 		attachLeaderboardHandler();
+		attachStartGameHandler();
+		attachSignUpHandler();
+
 	};
 
 	return {
