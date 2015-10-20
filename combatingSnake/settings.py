c217f9e579278f68027897b9288ee520dd2d1786
trevorta <tahientriet@gmail.com>
2015-10-20 15:19:02 -0700

    use postgres

--- combatingSnake/settings.py
+++ combatingSnake/settings.py
@@ -106,9 +106,9 @@ STATIC_URL = '/static/'
 
 # Yifan removed the following lines because it overrides the configs above
 # Parse database configuration from $DATABASE_URL
-#import dj_database_url
+import dj_database_url
 # FIXME: ADD CODE TO EXECUTE ONLY IN PRODUCTION
-#DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
