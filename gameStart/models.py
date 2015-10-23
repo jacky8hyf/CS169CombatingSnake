877b5cc555a1adadca2c6d0978d085a74adac283
trevorta <tahientriet@gmail.com>
2015-10-23 14:28:21 -0700

    update field size

--- gameStart/models.py
+++ gameStart/models.py
@@ -39,11 +39,11 @@ class BaseModel(models.Model):
 class User(BaseModel):
     # In views.py, use strId instead.
     userId = models.AutoField(primary_key=True)
-    username = models.CharField(max_length = 64, unique = True)
-    pwhash = models.CharField(max_length = 69)
-    nickname = models.CharField(max_length = 64)
+    username = models.CharField(max_length = 100, unique = True)
+    pwhash = models.CharField(max_length = 100)
+    nickname = models.CharField(max_length = 100)
     inroom = models.ForeignKey('Room', default = None, null = True, on_delete=models.SET_NULL)
-    session_id = models.CharField(max_length = 32, default = None, null = True, unique = True)
+    session_id = models.CharField(max_length = 100, default = None, null = True, unique = True)
 
     @property
     def password(self):
