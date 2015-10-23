29cac706d8e668c4aa5bedc33f2583b98abb0c91
Elsk Hong <jacky8hyf@gmail.com>
2015-10-23 00:55:11 +0000

    revert views.py as before merging

--- gameStart/views.py
+++ gameStart/views.py
@@ -58,8 +58,11 @@ class UsersView(View):
         jsonbody = parse_json(request.body)
         try:
             user = User.from_dict(jsonbody).login().save();
-        except IntegrityError:
-            return errors.USERNAME_TAKEN
+        except IntegrityError as e:
+            string = str(e)
+            if 'UNIQUE' in string and 'username' in string: # try to identify the error
+                return errors.USERNAME_TAKEN
+            return errors.INTERNAL_SERVER_ERROR('{}: {}'.format(type(e).__name__, string))
         return OKResponse(userId = user.strId, sessionId = user.session_id)
 
 class UsersLoginView(View):
@@ -73,7 +76,7 @@ class UsersLoginView(View):
         try:
             user = User.find_by_username(username)
         except User.DoesNotExist:
-            return errors.USERNAME_NOT_VALID('cannot find user: {}'.format(username))
+            return errors.USERNAME_NOT_VALID('cannot find user {}'.format(username))
         if not user.check_password(password):
             return errors.INCORRECT_PASSWORD
         user.login().save()
@@ -170,12 +173,9 @@ class SingleRoomSingleMemberView(View):
         user = fetch_user(request)
         if memberId != user.strId:
             return errors.PERMISSION_DENIED
-        room = Room.find_by_id(str(roomId))
-        if len(room.all_members) < room.capacity - 1: # -1 for the creator
-            user.enter_room(room).save()
-            return OKResponse()
-        else:
-            return errors.ROOM_FULL
+        room = Room.find_by_id(str(roomId)).raise_if_cannot_join(user)
+        user.enter_room(room).save()
+        return OKResponse()
 
     @transaction.atomic
     def delete(self, request, roomId, memberId, *args, **kwargs):
