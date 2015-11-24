602b06d14265547482d9f347d8ef711542fcf067
trevorta <tahientriet@gmail.com>
2015-11-24 14:24:14 -0800

    Fixed snake eating itself not showing color, leave room problems.

--- gameStart/static/js/user.js
+++ gameStart/static/js/user.js
@@ -416,14 +416,6 @@ var UserHandler = (function() {
         });
     };
 
-    //var attachAvailableRoomOnClickHandler = function(e){
-    //    for (room in roomList){
-    //        $('body').on('click', '#listofrooms option[value='+ room +']', function(e){
-    //            //e.preventDefault();
-    //            joinAvailableRoom(roomList[e.target.value]);
-    //        });
-    //    }
-    //};
     var attachAvailableRoomOnClickHandler = function(e){
         $('body').on('click', '#listofrooms',function(e){
             var id = $('#listofrooms').val();
@@ -514,7 +506,7 @@ var UserHandler = (function() {
                 players.html('');
                 var player = $(playerHtmlTemplate);
                 player.attr("id", dict.creator.userId);  // add id so we can update health field later
-                player.find('.name').text(available_room.creator.nickname);
+                player.find('.name').text(dict.creator.nickname);
                 user_color_map[dict.creator.userId] = color_lookup[players.size()];
                 player.addClass(color_lookup[1]);
                 players.append(player);
@@ -571,6 +563,10 @@ var UserHandler = (function() {
         });
     };
 
+    /**
+     * Send the keystroke to the server using websocket.
+     * @param e
+     */
     var sendKeyStroke = function(e) {
         if (inbox != null && gameStarted) {
             var msg;
@@ -603,8 +599,20 @@ var UserHandler = (function() {
         }
     };
 
+    /**
+     * Draw the snakes in @param snakes onto the board.
+     * @param snakes
+     */
     var drawSnakes = function(snakes) {
-        removeSnakes();
+        // Remove old snakes on the board.
+        for (var key in old_snakes_state){
+            var snake_body = old_snakes_state[key];
+            for (var i = 0; i < snake_body.length; i++) {
+                id = "r" + snake_body[i][0] + "c" + snake_body[i][1];
+                $("#" + id).removeClass(user_color_map[key]);
+            }
+        }
+        // Add in new snakes.
         for (var key in snakes){
             if (key != '_food') {
                 var snake_body = snakes[key];
@@ -617,22 +625,18 @@ var UserHandler = (function() {
         old_snakes_state = snakes;
     };
 
-    var removeSnakes = function() {
-        for (var key in old_snakes_state){
-            var snake_body = old_snakes_state[key];
-            for (var i = 0; i < snake_body.length; i++) {
-                id = "r" + snake_body[i][0] + "c" + snake_body[i][1];
-                $("#" + id).removeClass(user_color_map[key]);
-            }
-        }
-    };
-
     /**
      * Draw all foods onto the board.
      * @param foods : new foods
      */
     var drawFoods = function(foods) {
-        removeFoods();
+        // Remove the old foods from the board
+        for (var i = 0; i < old_foods.length; i++){
+            var food = old_foods[i];
+            id = "r" + food[0] + "c" + food[1];
+            $("#" + id).removeClass("food");
+        }
+        // add in the new foods
         for (var i = 0; i < foods.length; i++) {
             var food = foods[i];
             id = "r" + food[0] + "c" + food[1];
@@ -642,17 +646,8 @@ var UserHandler = (function() {
     };
 
     /**
-     * Remove all old foods
+     * Draw the initial board.
      */
-    var removeFoods = function() {
-        for (var i = 0; i < old_foods.length; i++){
-            var food = old_foods[i];
-            id = "r" + food[0] + "c" + food[1];
-            $("#" + id).removeClass("food");
-        }
-    };
-
-
     var setBoard = function() {
         var table = "<table>";
         for(var i=0; i < nRows; i ++) {
